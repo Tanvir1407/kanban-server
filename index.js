788535a09723ec4d74da94d22deb8fc2275c6087
MD. Tanvir <mt14050@gmail.com>
2023-09-07 13:26:41 +0600

    ..

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 const port = process.env.PORT || 5500;
 
 app.use(cors({
-  origin:["https://kanban-client-psi.vercel.app/"],
+  origin:["*"],
   methods:["GET","OPTIONS","DELETE","POST","PUT"],
   credentials: true
 }))
